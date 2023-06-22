aac1f0499ce6d5ebd788ce81897868218b5d98dc
KKKHun2 <1224kim2son@naver.com>
2023-06-22 23:27:17 +0900

    🎉 프로젝트 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/book",
+        destination: `https://books-api.nomadcoders.workers.dev/lists`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
